fac3dc70878f802e091132e85827ba6a8bc4fe69
Fajar Yudha Pratama <fajaryudha23@gmail.com>
2017-01-18 04:38:01 +0700

    RecyclerView Search

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recycleview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recycleview3/MainActivity.java
@@ -1,4 +1,4 @@
-package id.sch.smktelkom_mlg.learn.recycleview3;
+package id.sch.smktelkom_mlg.learn.recyclerview3;
 
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -7,17 +7,22 @@ import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.annotation.IntegerRes;
+import android.support.annotation.StringDef;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
+
 import id.sch.smktelkom_mlg.learn.recycleview3.DetailActivity;
 import id.sch.smktelkom_mlg.learn.recycleview3.InputActivity;
 import id.sch.smktelkom_mlg.learn.recycleview3.adapter.HotelAdapter;
@@ -27,11 +32,15 @@ import id.sch.smktelkom_mlg.learn.recycleview3.model.Hotel;
 public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter {
     public static final String HOTEL = "hotel";
     public static final int REQUEST_CODE_ADD = 88;
-        public static final int REQUEST_CODE_EDIT = 99;
-        int itemPos;
+    public static final int REQUEST_CODE_EDIT = 99;
+    int itemPos;
 
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
+        ArrayList<Hotel> mListAll = new ArrayList<>();
+        boolean isFiltered;
+        ArrayList<Integer> mListMapFilter = new ArrayList<>();
+        String mQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,12 +76,16 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         if(requestCode == REQUEST_CODE_ADD && resultCode == RESULT_OK){
             Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
             mList.add(hotel);
+                        //mAdapter.notifyDataSetChanged();
+                                if(isFiltered) mListAll.add(hotel);
+                        doFilter(mQuery);
+        } else if (requestCode == REQUEST_CODE_EDIT && resultCode == RESULT_OK){
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.remove(itemPos);
+                       if (isFiltered) mListAll.remove(mListMapFilter.get(itemPos).intValue());
+                        mList.add(itemPos, hotel);
+                       if(isFiltered) mListAll.add(mListMapFilter.get(itemPos), hotel);
             mAdapter.notifyDataSetChanged();
-                    } else if (requestCode == REQUEST_CODE_EDIT && resultCode == RESULT_OK){
-                        Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
-                        mList.remove(itemPos);
-                        mList.add(itemPos,hotel);
-                        mAdapter.notifyDataSetChanged();
 
         }
     }
@@ -106,8 +119,54 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+                       MenuItem searchItem = menu.findItem(R.id.action_search);
+                SearchView searchView = (SearchView)
+                              MenuItemCompat.getActionView(searchItem);
+
+                        searchView.setOnQueryTextListener(
+                                       new SearchView.OnQueryTextListener()
+                                        {
+                                            @Override
+                    public boolean onQueryTextSubmit(String query){
+                            return false;
+                        }
+
+                            @Override
+                    public boolean onQueryTextChange(String newText){
+                            mQuery = newText.toLowerCase();
+                            doFilter(mQuery);
+                            return true;
+                        }
+                });
         return true;
     }
+
+        private void doFilter(String query) {
+                if (!isFiltered){
+                        mListAll.clear();
+                        mListAll.addAll(mList);
+                        isFiltered = true;
+                    }
+
+                        mList.clear();
+                if (query==null||query.isEmpty()){
+                        mList.addAll(mListAll);
+                        isFiltered = false;
+                    } else {
+                        mListMapFilter.clear();
+                        for (int i = 0; i < mListAll.size(); i++){
+                                Hotel hotel = mListAll.get(i);
+                                if (hotel.judul.toLowerCase().contains(query)||
+                                                hotel.deskripsi.toLowerCase().contains(query)||
+                                                hotel.lokasi.toLowerCase().contains(query)){
+                                        mList.add(hotel);
+                                        mListMapFilter.add(i);
+                                    }
+                            }
+                    }
+                mAdapter.notifyDataSetChanged();
+            }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -131,39 +190,41 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         startActivity(intent);
     }
 
-               @Override
-        public void doEdit(int pos) {
-                itemPos = pos;
-                Intent intent = new Intent(this, InputActivity.class);
-                intent.putExtra(HOTEL, mList.get(pos));
-                startActivityForResult(intent, REQUEST_CODE_EDIT);
+    @Override
+    public void doEdit(int pos) {
+        itemPos = pos;
+        Intent intent = new Intent(this, InputActivity.class);
+        intent.putExtra(HOTEL, mList.get(pos));
+        startActivityForResult(intent, REQUEST_CODE_EDIT);
 
+    }
+
+    @Override
+    public void doDelete(int pos) {
+        itemPos = pos;
+        final  Hotel hotel = mList.get(pos);
+        mList.remove(itemPos);
+                if (isFiltered) mListAll.remove(mListMapFilter.get(itemPos).intValue());
+        mAdapter.notifyDataSetChanged();
+        Snackbar.make(findViewById(R.id.fab),hotel.judul+" Terhapus",Snackbar.LENGTH_LONG)
+                .setAction("UNDO", new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        mList.add(itemPos,hotel);
+                                                if (isFiltered) mListAll.add(mListMapFilter.get(itemPos), hotel);
+                        mAdapter.notifyDataSetChanged();
                     }
+                })
+                .show();
+    }
 
-                @Override
-        public void doDelete(int pos) {
-                    itemPos = pos;
-                            final  Hotel hotel = mList.get(pos);
-                            mList.remove(itemPos);
-                            mAdapter.notifyDataSetChanged();
-                            Snackbar.make(findViewById(R.id.fab),hotel.judul+" Terhapus",Snackbar.LENGTH_LONG)
-                                            .setAction("UNDO", new View.OnClickListener(){
-                                            @Override
-                                            public void onClick(View v){
-                                                    mList.add(itemPos,hotel);
-                                                    mAdapter.notifyDataSetChanged();
-                                                }
-                                        })
-                                            .show();
-                    }
+    @Override
+    public void doFav(int pos) {
 
-                @Override
-        public void doFav(int pos) {
+    }
 
-                    }
+    @Override
+    public void doShare(int pos) {
 
-                @Override
-        public void doShare(int pos) {
-
-                    }
+    }
 }
